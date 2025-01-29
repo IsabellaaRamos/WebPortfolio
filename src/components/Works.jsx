f05f5719a6ff25899991d21c028a481bb9962208
Isabellaaramos <Isabellaramos7@outlook.com>
2025-01-29 16:13:31 -0300

    melhora estilos dos componentes About e Works, adiciona sombras e altera cores de fundo

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -25,7 +25,7 @@ const ProjectCard = ({
           scale: 1,
           speed: 450,
         }}
-        className='bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full'
+        className='bg-violet-900 bg-opacity-50 shadow-2xl shadow-purple-800 p-5 rounded-2xl sm:w-[360px] w-full'
       >
         <div className='relative w-full h-[230px]'>
           <img
